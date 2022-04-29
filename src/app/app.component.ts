f648f8a6a8e85ad06b950fd35f6cfff3fa409aaf
K0966014 <nattawat.k@kbtg.tech>
2022-04-29 13:22:11 +0700

    Send data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-angular';
-  name = 'Nutz Allstar';
-  names = [];
-  datas = [
-    { id: 1, name: 'nutzallstar' },
-    { id: 2, name: 'nutz' }
-  ];
 
-  doSth(newName: string) {
-    console.log(newName);
-    this.name = newName;
-  }
+  parentName = { id: 1, name: 'nutz' };
+
 }
--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-other',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./other.component.css']
 })
 export class OtherComponent implements OnInit {
+
+  @Input() name = {};
 
   constructor() { }
 
